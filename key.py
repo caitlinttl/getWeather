3197764fd97b264b33cf7d85099d9cec23715be2
tzuling.tseng <riarys16@gmail.com>
2021-12-10 11:17:51 +0800

    [test] import pyd error

---
+++ key.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+# Created by Caitlin, Hsinchu, Taiwan, 2021.
+# Get IFTTT token
+# ----------------------------------------------------------
+
+def getToken():
+    return "cgcFQMktW46bFG9exR5nta"
+
